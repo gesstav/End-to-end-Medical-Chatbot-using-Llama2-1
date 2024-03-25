9501b624993884be8d4b65b5c98e020ec19b0e27
gesstav <64750852+gesstav@users.noreply.github.com>
2024-03-25 19:23:41 +0000

    commit

--- store_index.py
+++ store_index.py
@@ -6,8 +6,8 @@ import os
 
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+PINECONE_API_KEY = os.environ.get('f0d9f7f3-b513-4bfa-bbaa-e803494d8e01')
+PINECONE_API_ENV = os.environ.get('gcp-starter')
 
 # print(PINECONE_API_KEY)
 # print(PINECONE_API_ENV)
@@ -18,11 +18,29 @@ embeddings = download_hugging_face_embeddings()
 
 
 #Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
+import os
+from pinecone import Pinecone, ServerlessSpec
+
+pc = Pinecone(
+        api_key=os.environ.get("PINECONE_API_KEY")
+    )
+
+    # Now do stuff
+if 'my_index' not in pc.list_indexes().names():
+        pc.create_index(
+            name='my_index', 
+            dimension=1536, 
+            metric='euclidean',
+            spec=ServerlessSpec(
+                cloud='aws',
+                region='us-west-2'
+            )
+        )
 
 
-index_name="medical-bot"
+
+
+index_name="medical-chatbot"
 
 #Creating Embeddings for Each of The Text Chunks & storing
 docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
